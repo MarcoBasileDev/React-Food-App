4b868443a4a06fc1d44d641744923f40af5b9765
MarcoBasileDev <marco.basile1996@gmail.com>
2024-04-18 15:22:36 +0200

    add new pages

--- app/page.js
+++ app/page.js
@@ -1,12 +1,19 @@
 import Link from "next/link";
-import Header from "@/components/header";
 
 export default function Home() {
   return (
     <main>
-      <Header />
       <p>
-        <Link href="/about">About Us</Link>
+        <Link href="/meals">Meals</Link>
+      </p>
+      <p>
+        <Link href="/meals/share">Share Meals</Link>
+      </p>
+      <p>
+        <Link href="/community">Community</Link>
+      </p>
+      <p>
+        <Link href="/meals/type1">meals type1</Link>
       </p>
     </main>
   );
